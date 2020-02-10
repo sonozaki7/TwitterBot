4b50d2339a2b0ec93f448b225a63691caa9cdf79
So Nozaki <sou.nozaki@gmail.com>
2020-02-09 21:49:50 -0800

    implemented bot code

---
+++ twbot.py
@@ -0,0 +1,25 @@
+from auth import(
+    auth_consumer_key,
+    auth_consumer_secret,
+    auth_access_token,
+    auth_access_token_secret
+)
+
+var Twit = require('twit')
+var T = new Twit({
+    consumer_key:         auth_consumer_key,
+    consumer_secret:      auth_consumer_secret,
+    access_token:         auth_access_token,
+    access_token_secret:  auth_access_token_secret,
+})
+var users = ["So_Nozaki"];
+var stream = T.stream('statuses/filter', {follow: users});
+stream.on('tweet', function (tweet) {
+    if (users.indexOf(tweet.user.id_str) > -1) {
+        console.log(tweet.user.name + ": " + tweet.text);
+        T.post('statuses/retweet/:id', { id: tweet.id_str },
+               function (err, data, response) {
+            console.log(data)
+        })
+    }
+})
